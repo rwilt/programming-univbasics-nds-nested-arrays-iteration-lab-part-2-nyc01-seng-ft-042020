fc2c098ae64301f567332402b90cb189be31afe7
rosie w <rosie.wilt@gmail.com>
2020-04-09 15:39:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -26,41 +26,35 @@ def find_min_in_nested_arrays(src)
   src_index = 0
   while src_index < src.length do
   src_element_index = 0
-  min_val = 0
+  
   while src_element_index < src[src_index].length do
-
+    min_val = src[src_index][element_index][0]
+    if src[src_index][element_index] < min_val
+    min_val = src[src_index][element_index]
+    new_arr.push(minVal)
+  end 
+  src_element_index += 1
 end
+src_index += 1 
 end
+new_arr
 
-#LEARN EXAMPLE BELOW
-
-spice_rack = [
-  ["Posh", "Scary", "Sporty"],
-  ["Paprika", "Fajita Mix", "Coriander"],
-  ["Parsley", "Sage", "Rosemary"]
-]
-
-outer_results = []
+def find_min_in_nested_arrays(src)
+final_results = []
 row_index = 0
-while row_index < spice_rack.count do
+while row_index < src.count do
   element_index = 0
-  longest_string_element = ""
-  while element_index < spice_rack[row_index].count do
+  min_val = src[row_index][0]
+  puts "min val is #{{min_val}}"
+  while element_index < src[row_index].count do
 
-    # Array at row_index
-    # Element of the inner array at element_index
-    # If the length of the current element is greater than the length of longest_string_element
-    # Set longest_string_element to the current element
-    if spice_rack[row_index][element_index].length > longest_string_element.length
-      longest_string_element = spice_rack[row_index][element_index]
+    if src[row_index][element_index] < min_val
+      min_val = src[row_index][element_index]
     end
     element_index += 1
   end
 
-  # We have to check every element in each inner array, so after the loop finishes
-  # The current value of longest_string_element is pushed into outer_results
-  outer_results << longest_string_element
+  final_results << min_val
   row_index += 1
 end
-
-outer_results # => ["Sporty", "Fajita Mix", "Rosemary"]
+ 
